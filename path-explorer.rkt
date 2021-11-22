92c8b2e128e62f40648f223e4b4967f80fb8fcc7
Giuliano Losa <giuliano@losa.fr>
2021-11-22 15:03:32 -0800

    Add bare-bones prototype
    The general idea is to construct a Rosette program that takes a
    generator as input and then then follows the path as given by the
    generator.

---
+++ path-explorer.rkt
@@ -0,0 +1,34 @@
+#lang rosette
+
+(require racket/generator)
+(require (for-syntax syntax/parse)
+         syntax/parse/define)
+
+; we'll use a generator to produce numbers that encode which branch to take
+(define g (generator () 0))
+
+; now let's write a macro that takes a program and creates a Rosette program that follows the path given by the generator
+(define-syntax (define-path-explorer stx)
+  (syntax-parse stx
+    [(_ (name arg0 ...) body)
+     #'(define (name arg0 ...)
+         (path-explorer body))]))
+
+(define-syntax (path-explorer stx)
+  (syntax-parse stx
+    [(_ (if-id:id c then-branch else-branch))
+     #`(if (equal? (g) 0)
+        (begin
+         (assume c)
+         then-branch)
+        (begin
+          (assume (! c))
+          else-branch))]
+    [(_ (x ...))
+     #'(begin
+         (println "ha")
+         (x ...))]))
+
+(define-symbolic b boolean?)
+(define-path-explorer (test cond) (if cond #f #t))
+(solve (test b))
