4920327e391b4a546e1f11c3d2c04d90ba258df4
Giuliano Losa <giuliano@losa.fr>
2021-11-22 15:59:01 -0800

    fix the generator

--- path-explorer.rkt
+++ path-explorer.rkt
@@ -5,7 +5,18 @@
          syntax/parse/define)
 
 ; we'll use a generator to produce numbers that encode which branch to take
-(define g (generator () 0))
+#;(define g
+  (generator (n)
+             (let loop ()
+               (begin
+                 (yield 0)
+                 (loop)))))
+(define g
+  (generator (n)
+             (let loop ()
+               (begin
+                 (yield (random n))
+                 (loop)))))
 
 ; now let's write a macro that takes a program and creates a Rosette program that follows the path given by the generator
 (define-syntax (define-path-explorer stx)
@@ -14,21 +25,34 @@
      #'(define (name arg0 ...)
          (path-explorer body))]))
 
+; TODO better trace prints
 (define-syntax (path-explorer stx)
   (syntax-parse stx
-    [(_ (literal c then-branch else-branch)) ; TODO: how to match only the "real" if?
-     #`(if (equal? (g) 0)
-        (begin
-         (assume c)
-         then-branch)
-        (begin
-          (assume (! c))
-          else-branch))]
+    #:literals (if cond)
+    [(_ (if c then-branch else-branch))
+     #'(let ([branch (g 2)])
+         (if (equal? branch 0)
+             (begin
+               (println (format "branch ~a; assuming: ~a" branch (syntax->datum #'c)))
+               (assume c)
+               (path-explorer then-branch))
+             (begin
+               (println (format "branch ~a; assuming: ~a" branch #'(! c)))
+               (assume (! c))
+               (path-explorer else-branch))))]
+; TODO is there a "default" mechanism for this?
     [(_ (x ...))
      #'(begin
-         (println "ha")
-         (x ...))]))
+         (x ...))]
+    [(_ x)
+     #'(begin
+         x)]))
 
-(define-symbolic b boolean?)
-(define-path-explorer (test cond) (if cond #f #t))
-(solve (test b))
+(define-symbolic i integer?)
+(define-path-explorer (test i) (if (<= 0 i) (if (<= 1 i) 'strict-pos 'zero) 'neg))
+
+;(test 0)
+
+
+; this gives us an input that satisfies the path condition given by the generator.
+(solve (test i))
