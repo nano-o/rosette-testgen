7021ffad6f3d143b589228628d243a2cd44f253a
Giuliano Losa <giuliano@losa.fr>
2021-11-23 11:32:43 -0800

    add cond case

--- path-explorer.rkt
+++ path-explorer.rkt
@@ -5,18 +5,20 @@
          syntax/parse/define)
 
 ; we'll use a generator to produce numbers that encode which branch to take
-#;(define g
+(define constant-gen-0
   (generator (n)
              (let loop ()
                (begin
                  (yield 0)
                  (loop)))))
-(define g
+(define random-gen
   (generator (n)
              (let loop ()
                (begin
                  (yield (random n))
                  (loop)))))
+
+(define g random-gen)
 
 ; now let's write a macro that takes a program and creates a Rosette program that follows the path given by the generator
 (define-syntax (define-path-explorer stx)
@@ -25,7 +27,9 @@
      #'(define (name arg0 ...)
          (path-explorer body))]))
 
-; TODO better trace prints
+(define (print-branch branch c)
+  (println (format "branch ~a; assuming: ~a" branch c)))
+
 (define-syntax (path-explorer stx)
   (syntax-parse stx
     #:literals (if cond)
@@ -33,26 +37,37 @@
      #'(let ([branch (g 2)])
          (if (equal? branch 0)
              (begin
-               (println (format "branch ~a; assuming: ~a" branch (syntax->datum #'c)))
+               (print-branch branch (syntax->datum #'c)) ; TODO how to make this a macro?
                (assume c)
                (path-explorer then-branch))
              (begin
-               (println (format "branch ~a; assuming: ~a" branch #'(! c)))
+               (print-branch branch (syntax->datum #'(! c)))
                (assume (! c))
                (path-explorer else-branch))))]
-; TODO is there a "default" mechanism for this?
-    [(_ (x ...))
-     #'(begin
-         (x ...))]
-    [(_ x)
-     #'(begin
-         x)]))
+    [(_ (cond ([c0 body0] ...)))
+     (with-syntax ([how-many (length (syntax->list #'(c0 ...)))])
+     #'(let ([branch (g how-many)])
+         (begin
+           (assume (list-ref (list c0 ...) branch))
+           (list-ref (list body0 ...) branch))))]
+; TODO is there a "default" mechanism for the following?
+    [(_ (x ...)) #'(x ...)]
+    [(_ x) #'x]))
 
 (define-symbolic i integer?)
-(define-path-explorer (test i) (if (<= 0 i) (if (<= 1 i) 'strict-pos 'zero) 'neg))
 
-;(test 0)
+;(define-path-explorer (test-1 i) (if (<= 0 i) (if (<= 1 i) 'strict-pos 'zero) 'neg))
+
+(define-path-explorer (test-2 i)
+  (cond ([(< 0 i) 'strict-pos]
+         [(equal? 0 i) 'zero]
+         [(< i 0) 'neg])))
+
+;(test-1 0)
+;(test-2 0)
 
 
 ; this gives us an input that satisfies the path condition given by the generator.
-(solve (test i))
+;(solve (test-1 i))
+(solve (test-2 i))
+
