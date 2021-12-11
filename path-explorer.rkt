5731ac48a56d4d1e81571db03045228ccbda6b25
Giuliano Losa <giuliano@losa.fr>
2021-12-10 17:37:27 -0800

    wip: support auxiliary definition

--- path-explorer.rkt
+++ path-explorer.rkt
@@ -1,12 +1,12 @@
 #lang rosette
 
-(require (for-syntax syntax/parse racket/string racket/syntax)
-         racket/generator syntax/parse/define rosette/lib/destruct racket/stxparam rackunit "./generators.rkt")
 ; TODO detect symbols that have a path-explorer already and call that
 ; TODO support struct methods implementing generic interfaces (i.e. find out all possible targets and branch on that)
 ; TODO is it worth trying to prune infeasible paths?
 ; TODO what about definitions comming from libraries?
 
+(require (for-syntax syntax/parse racket/syntax)
+         racket/generator rosette/lib/destruct racket/stxparam rackunit "./generators.rkt" syntax/parse)
 
 (provide define-with-path-explorer)
 
@@ -15,16 +15,19 @@
 ; it seems that we're only poluting this file though since we don't export g
 
 ; now let's write a macro that takes a racket definition and creates a Rosette program that follows the path given by a generator
+
+(define-for-syntax (explorer-id x)
+  (format-id x "~a-path-explorer" x))
+
 (define-syntax (define-with-path-explorer stx)
   (syntax-parse stx
     [(_ (name:id arg0:id ...) body)
-     (with-syntax ([explorer (format-id #'name "~a-path-explorer" #'name)])
-       #'(begin
-           (define (explorer gen arg0 ...)
-             (syntax-parameterize ([g (make-rename-transformer #'gen)])
-               (path-explorer body))) ; TODO should we just pass the generator to path-explorer? but then all recursive expansions of path-explorer have to get the gen
-           (define (name arg0 ...)
-             body)))]))
+     #`(begin
+         (define (#,(explorer-id #'name) gen arg0 ...)
+           (syntax-parameterize ([g (make-rename-transformer #'gen)])
+             (path-explorer body))) ; TODO should we just pass the generator to path-explorer? but then all recursive expansions of path-explorer have to get the gen
+         (define (name arg0 ...)
+           body))]))
 
 (define (print-branch branch c)
   (println (format "branch ~a; assuming: ~a" branch c)))
@@ -32,7 +35,7 @@
 (define-for-syntax debug? #t) ; TODO: should this be a syntax parameter? can we set! it?
 ;(define-syntax debug? #t) ; NOTE this works! seems that there's no clash because that's not the same level.
 
-(define-syntax (path-explorer stx) ; TODO detect symbols that have a path-explorer already and call that
+(define-syntax (path-explorer stx)
   (define (from-to i n)
     (if (<= i n) (cons i (from-to (+ i 1) n)) null))
   (define (syntax->string-list stx)
@@ -45,20 +48,22 @@
     (if debug?
         (println i)
         (values)))
+  (define-syntax-class (has-path-explorer)
+    (pattern x:id #:when (identifier-binding (explorer-id #'x))))
   
   (syntax-parse stx
     #:track-literals ; per advice here:  https://school.racket-lang.org/2019/plan/tue-aft-lecture.html
     [(_ ((~datum if) c then-branch else-branch)) ; TODO what about recursing in the condition?
      (print-debug-info "if")
-     #`(let ([branch (g 2)])
+     #`(let ([branch (g 2)] [cond (path-explorer c)])
          (if (equal? branch 0)
              (begin
                #,(print-branch-condition #'branch #'c)
-               (assume c)
+               (assume cond)
                (path-explorer then-branch))
              (begin
                #,(print-branch-condition #'branch #'(! c))
-               (assume (! c))
+               (assume (! cond))
                (path-explorer else-branch))))]
     [(_ ((~datum cond) [c0 body0] ... [(~datum else) ~! else-body]))
      (print-debug-info "cond with else")
@@ -93,11 +98,11 @@
                       (path-explorer body0))
                     (assume #f))]
                ...))]))]
-; TODO the following is messy
     [(_ ((~datum lambda) (arg0 ...) body) (~do (print-debug-info "lambda"))) #'(lambda (arg0 ...) (path-explorer body))]
     [(_ ((~datum λ) (arg0 ...) body (~do (print-debug-info "λ")))) #'(lambda (arg0 ...) (path-explorer body))]
     [(_ (x:keyword arg0 ...) (~do (print-debug-info "keyword"))) #'(x arg0 ...)]
     [(_ ((~datum quote) arg0 ...) (~do (print-debug-info "quote"))) #'(quote arg0 ...)]
+    [(_ (fn:has-path-explorer arg0 ...) (~do (print-debug-info "path-explorer application"))) #`(#,(explorer-id #'fn) (path-explorer arg0) ...)]
     [(_ (fn arg0 ...) (~do (print-debug-info "application"))) #'((path-explorer fn) (path-explorer arg0) ...)]
     [(_ x:integer (~do (print-debug-info "integer"))) #'x]
     [(_ x:boolean (~do (print-debug-info "integer"))) #'x]
@@ -107,6 +112,11 @@
 ; tests
 
 (define-with-path-explorer (test-if i) (if (<= 0 i) (if (<= 1 i) 'strict-pos 'zero) 'neg))
+(define-with-path-explorer (test1 i) (if (equal? (test-if i) 'neg) 'neg 'pos))(define-symbolic i integer?)
+(test1-path-explorer (constant-gen 0) 2)
+#;(let ([model (solve (test1-path-explorer (constant-gen 0) i))])
+  (check-equal? (< 0 (evaluate i model)) #t))
+#|
 (define-symbolic i integer?)
 (let ([model (solve (test-if-path-explorer (constant-gen 0) i))])
   (check-equal? (< 0 (evaluate i model)) #t))
@@ -141,5 +151,6 @@
 
 (let ([model (solve (test-destruct-path-explorer (constant-gen 1) in))])
   (check-equal? (evaluate x model) #f))
+|#
 
 ; TODO test lambda etc. and test with definition with nested conditionals
