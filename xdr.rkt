b33fae8b90d388494eeeaeffa1903d491ae6a941
Giuliano Losa <giuliano@losa.fr>
2022-01-14 15:56:04 -0800

    fix lexical context in make-bv-type

--- xdr.rkt
+++ xdr.rkt
@@ -1,29 +1,37 @@
 #lang rosette
 
 (require racket/include)
-(require (for-syntax syntax/parse racket/syntax) syntax/parse macro-debugger/stepper)
-         
-(define-for-syntax base-types
+(require (for-syntax syntax/parse racket/syntax) macro-debugger/stepper)
+
+#;(define-for-syntax base-types
   (list "bool" "int" "unsigned int" "hyper" "unsigned hyper" "double" "quadruple" "float"))
 
 ; a bitvector type needs a type predicate and a constructor
 (define-syntax (make-bv-type stx)
   (syntax-parse stx
     [(_ name:id nbits:number)
-     (with-syntax ([type-pred (format-id stx "~a?" #'name)])
+     (with-syntax ([type-pred (format-id #'name "~a?" #'name)]) ; NOTE: we need the lexical context of #'name and not stx if this macro is to be used inside anoother macro...
      #`(begin
          (define type-pred (bitvector nbits))
-         (define (#,(format-id stx "~a" #'name) val) (bv val type-pred))))]))
+         (define (#,(format-id #'name "~a" #'name) val) (bv val type-pred))))]))
 
 (define-syntax (define-type stx)
   (syntax-parse stx
-    [(_ type:string ((~literal fixed-length-array) "opaque" nbits:nat))
-     (with-syntax ([type-id (format-id stx "~a" (syntax->datum #'type))])
-       #'(make-bv-type type-id nbits))]))
+    ; opaque fixe-length arrays
+    [(_ typename:string ((~literal fixed-length-array) "opaque" nbits:nat))
+     (with-syntax ([type-id (format-id stx "~a" (syntax-e #'typename))])
+       #'(make-bv-type type-id nbits))]
+    ; int
+    [(_ typename:string "int")
+     (with-syntax ([type-id (format-id stx "~a" (syntax-e #'typename))])
+       #'(make-bv-type type-id 32))]))
 
-#;(include (file "/home/nano/Documents/guile/Stellar.xdr.scm"))
+#;(include (file "./Stellar.xdr.scm"))
 
-(expand/step
- #'(define-type
-     "Hash"
-     (fixed-length-array "opaque" 32)))
+(define-type
+    "Hash"
+    (fixed-length-array "opaque" 32))
+(define-type "int32" "int")
+
+(Hash 1)
+(int32 0)
