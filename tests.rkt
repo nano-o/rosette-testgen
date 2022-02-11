cc8c73ffb0067880647f18645df1ed32c5c2eb8a
Giuliano Losa <giuliano@losa.fr>
2022-02-11 09:49:48 -0800

    run test suites

--- tests.rkt
+++ tests.rkt
@@ -1,5 +1,7 @@
-#lang rosette
+#lang racket
 
-(require "xdr-compiler.rkt" "guile-ast-example.rkt")
+(require "xdr-compiler.rkt" "guile-ast-example.rkt" rackunit)
 
 (run-test parse-asts-tests)
+(run-test ks-v-assoc->hash-tests)
+(run-test stellar-xdr-tests)
