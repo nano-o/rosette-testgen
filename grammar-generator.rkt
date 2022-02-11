46aa86d7f2d6f2e7c1e93113431c6783bc1b8b97
Giuliano Losa <giuliano@losa.fr>
2022-02-11 14:45:23 -0800

    Solve the grammar-rule sharing proglem

--- grammar-generator.rkt
+++ grammar-generator.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (provide test-grammar)
-(require "xdr-compiler.rkt" ;"guile-ast-example.rkt"
+(require
+  racket/match racket/syntax
+  "xdr-compiler.rkt" ;"guile-ast-example.rkt"
   (for-template rosette rosette/lib/synthax))
 
 ; TODO generate a Rosette grammar for this:
@@ -12,37 +14,50 @@
 ; a simpler example:
 (define test-ast
   #'((define-type
-      "uint256"
-      (fixed-length-array "opaque" 32))
-    (define-type
-      "PublicKeyType"
-      (enum ("PUBLIC_KEY_TYPE_ED25519" 0)))
-    (define-type
-      "PublicKey"
-      (union (case ("type" "PublicKeyType")
-               (("PUBLIC_KEY_TYPE_ED25519") ("ed25519" "uint256")))))))
+       "uint256"
+       (fixed-length-array "opaque" 32))
+     (define-type
+       "my-array"
+       (fixed-length-array "uint256" 2))
+     (define-type
+       "PublicKeyType"
+       (enum ("PUBLIC_KEY_TYPE_ED25519" 0)))
+     (define-type
+       "PublicKey"
+       (union (case ("type" "PublicKeyType")
+                (("PUBLIC_KEY_TYPE_ED25519") ("ed25519" "uint256")))))))
 
 (define test-sym-table
   (parse-asts test-ast))
 
 ; Produce a syntax object defining a Rosette grammar
-; Note that we need a syntax-object to use as context, provided by the macro calling this, otherwise g will be out of scope for the code that follows. 
+; Note that we need a syntax-object to use as context, provided by the macro calling this, otherwise g will be out of scope for the code that follows.
+; Looks like it's better to postpone using define-grammar to the macro that will use xdr-types->grammar
 #;(define (xdr-types->grammar stx-context sym-table type)
   (define (the-grammar t)
     #`[my-rule (?? (bitvector 32))])
   #`(define-grammar (#,(format-id stx-context "g")) #,(the-grammar type)))
 
-; another version
+; Builds a syntax object containing a list of grammar rules
 (define (xdr-types->grammar sym-table type)
+  (define (rule-id str [index 0])
+    ; Rosette seems to be relying on source location information.
+    ; Since we want all grammar holes to be independent, we need to use a unique location each time.
+    (format-id #f "~a-rule" str #:source (make-srcloc (format "~a-rule:~a" str index) 1 0 1 0)))
+  (define (array type size)
+    #`(list
+       #,@(datum->syntax
+          #'()
+          (for/list ([i (in-range size)])
+            #`(#,(rule-id type i))))))
   (define (the-grammar t)
-    #`[my-rule (?? (bitvector 32))])
+    (match (hash-ref sym-table t)
+      [`(opaque-fixed-length-array ,nbytes)
+       #`([#,(rule-id t) (?? (bitvector #,(* nbytes 8)))])]
+      [`(fixed-length-array ,elem-type ,size)
+       (let ([top-rule #`[#,(rule-id t) #,(array elem-type size)]])
+         #`(#,top-rule #,@(the-grammar elem-type)))]))
   (the-grammar type))
 
 (define (test-grammar)
-  (xdr-types->grammar (hash) "PublicKey"))
-
-#;(define-grammar (gg)
-  [PublicKey-rule
-   (cons 0 (uint256-rule))]
-  [uint256-rule
-   (?? (bitvector 32))])
+  (xdr-types->grammar test-sym-table "my-array"))
--- test-grammar.rkt
+++ test-grammar.rkt
@@ -1,17 +1,29 @@
 #lang rosette
 
 (require
-  rosette/lib/synthax
+  rosette/lib/synthax  syntax/parse/define
   (for-syntax "grammar-generator.rkt" racket/syntax))
 
-(define-syntax (define-g stx)
-  #`(define-grammar (#,(format-id stx "g")) #,(test-grammar)))
+(define-syntax-parser make-test-grammar
+  [(_ i:id)
+   #`(define-grammar (i) #,@(test-grammar))])
 
-(define-g)
+#;(define-syntax (define-g stx)
+  #`(define-grammar (#,(format-id stx "g")) #,@(test-grammar)))
+
+(make-test-grammar g)
+
+
+#;(define-grammar (g2) [my-array-rule (list (uint256-rule) (uint256-rule))] [uint256-rule (?? (bitvector 256))])
 
 (define sol
   (synthesize
    #:forall '()
-   #:guarantee (assert (equal? (g #:depth 1) (bv 1 32)))))
+   #:guarantee (assert (equal? (g #:depth 2) (list (bv 2 256) (bv 1 256))))))
+
+#;(define sol2
+  (synthesize
+   #:forall '()
+   #:guarantee (assert (equal? (g2 #:depth 2) (list (bv 2 256) (bv 1 256))))))
 
 (generate-forms sol)
