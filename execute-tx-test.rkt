824ca773a161734eb8c1fe636318e997970963ac
Giuliano Losa <giuliano@losa.fr>
2022-03-01 09:32:56 -0800

    obtain complete solutions

--- execute-tx-test.rkt
+++ execute-tx-test.rkt
@@ -3,19 +3,25 @@
 (require
   "Stellar-inline.rkt"
   "path-explorer.rkt"
-  rosette/lib/synthax)
+  rosette/lib/synthax
+  syntax/to-string)
 
-(define min-fee (bv 2 32))
+(define min-fee (bv 2 32)) ; TODO
 
 (define-with-path-explorer (execute-tx ledger time tx)
   (if (equal? (car tx) (bv ENVELOPE_TYPE_TX 32))
       (if (bvuge (Transaction-fee (TransactionV1Envelope-tx (cdr tx))) min-fee) 'okay 'low-fee)
       (error "error")))
 
-(define model-list (stream->list (all-paths (λ (gen) (execute-tx-path-explorer gen null null (TransactionEnvelope-grammar #:depth 4))))))
+(define input-tx (TransactionEnvelope-grammar #:depth 6))
 
-(for ([m model-list])
-  (println m))
+(define solution-list (stream->list (all-paths (λ (gen) (execute-tx-path-explorer gen null null input-tx)))))
+
+(for ([s solution-list])
+  (if (sat? s)
+      (let ([complete-sol (complete-solution s (symbolics input-tx))])
+        (map (λ (f) (pretty-display (syntax->datum f))) (generate-forms complete-sol)))
+      (void)))
 
 #|
 (define sol
