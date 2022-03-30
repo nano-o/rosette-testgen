2501c3acec2870a5ffcce516cfe711b9e6cc5a5e
Giuliano Losa <giuliano@losa.fr>
2022-03-30 13:51:22 -0700

    fix bug with bv

--- serialize.rkt
+++ serialize.rkt
@@ -5,12 +5,14 @@
   racket/syntax
   binaryio/integer
   syntax/to-string
+  (only-in rosette bv bitvector bv? bitvector->natural)
   "Stellar-grammar.rkt"
   "synthesized-tx-examples.rkt")
 
 (provide serialize)
 
 ; We serialize synthesized transactions and ledger entries to the representation expected by the guile-rpc library
+; TODO might be easier to eval the received syntax and then use match on the resulting datum
 
 ; we need the current namespace to do reflection on structs with eval
 (define-namespace-anchor a)
@@ -27,7 +29,7 @@
      (eval-syntax #'d.out ns)]))
 
 (define-syntax-class xdr-rep
-  #:datum-literals (list bv vector null bitvector :union: :byte-array:)
+  #:literals (list bv vector null bitvector :union: :byte-array:)
   [pattern (:union: tag:xdr-rep val:xdr-rep)
            #:attr out #`(cons tag.out val.out)]
   [pattern (:byte-array: (bv val:number size:number))
@@ -35,21 +37,20 @@
            #:attr out #`(bytes->list (integer->bytes val (/ size 8) #f #t))] ; unsigned big-endian
   [pattern (bv v:xdr-rep n:number)
            #:attr out #`v.out]
-  [pattern (list e*:xdr-rep ...)
-           ; fixed-size array
-           #:attr out #'`(,e*.out ...)]
+  [pattern bv-literal
+           #:when (bv? (syntax-e #'bv-literal))
+           #:attr out #`#,(bitvector->natural (syntax-e #'bv-literal))]
   [pattern (vector e*:xdr-rep ...)
            ; variable-size array
            #:attr out #'`#(,e*.out ...)]
-  [pattern (vector)
-           ; variable-size array of size 0
-           #:do (println "ha")
-           #:attr out #'`#()]
   [pattern (struct-name:id e*:xdr-rep ...)
            #:when (let ([type-id (format-id #'() "struct:~a" #'struct-name)])
                     (and
                      (identifier-binding type-id)
                      (struct-type? (eval-syntax type-id ns))))
+           #:attr out #'`(,e*.out ...)]
+  [pattern (e*:xdr-rep ...+)
+           ; fixed-size array
            #:attr out #'`(,e*.out ...)]
   [pattern n:number
            #:attr out #'n]
@@ -62,52 +63,53 @@
 (module+ test
   (require rackunit)
   (define example-1
-    #'(define my-tx
-        (:union:
-         (bv ENVELOPE_TYPE_TX 32)
-         (TransactionV1Envelope
-          (Transaction
-           (:union:
-            (bv KEY_TYPE_MUXED_ED25519 32)
-            (MuxedAccount::med25519
+    (let ([my-bv (bv 0 32)])
+      #`(define my-tx
+          (:union:
+           (bv ENVELOPE_TYPE_TX 32)
+           (TransactionV1Envelope
+            (Transaction
+             (:union:
+              (bv KEY_TYPE_MUXED_ED25519 32)
+              (MuxedAccount::med25519
+               (bv #x0000000000000000 64)
+               (:byte-array:
+                (bv
+                 29458565313587576488605812219632678825768279426807042594960959184304126581667
+                 256))))
+             #,my-bv
              (bv #x0000000000000000 64)
-             (:byte-array:
-              (bv
-               29458565313587576488605812219632678825768279426807042594960959184304126581667
-               256))))
-           (bv #x00000000 32)
-           (bv #x0000000000000000 64)
-           (:union: (bv FALSE 32) null)
-           (:union:
-            (bv MEMO_RETURN 32)
-            (:byte-array:
-             (bv #x0000000000000000000000000000000000000000000000000000000000000000 256)))
-           (vector
-            (Operation
              (:union: (bv FALSE 32) null)
              (:union:
-              (bv CREATE_ACCOUNT 32)
-              (CreateAccountOp
+              (bv MEMO_RETURN 32)
+              (:byte-array:
+               (bv #x0000000000000000000000000000000000000000000000000000000000000000 256)))
+             (vector
+              (Operation
+               (:union: (bv FALSE 32) null)
                (:union:
-                (bv PUBLIC_KEY_TYPE_ED25519 32)
-                (:byte-array:
-                 (bv
-                  29458565313587576488605812219632678825768279426807042594960959184304126581667
-                  256)))
-               (bv #x0000000000000000 64)))))
-           (:union: (bv 0 32) null))
-          (vector)))))
-  (define/provide-test-suite serialize/test
-    (test-case
-     "serialize transaction"
-     (check-equal?
-      (serialize example-1)
-      '(ENVELOPE-TYPE-TX
-        ((KEY-TYPE-MUXED-ED25519 0 (65 32 245 4 132 15 130 86 201 188 205 227 214 17 246 138 208 74 110 187 8 101 242 142 164 42 213 13 113 216 91 163))
-         0
-         0
-         (FALSE . void)
-         (MEMO-RETURN 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
-         #(((FALSE . void) (CREATE-ACCOUNT (PUBLIC-KEY-TYPE-ED25519 65 32 245 4 132 15 130 86 201 188 205 227 214 17 246 138 208 74 110 187 8 101 242 142 164 42 213 13 113 216 91 163) 0)))
-         (0 . void))
-        #())))))
+                (bv CREATE_ACCOUNT 32)
+                (CreateAccountOp
+                 (:union:
+                  (bv PUBLIC_KEY_TYPE_ED25519 32)
+                  (:byte-array:
+                   (bv
+                    29458565313587576488605812219632678825768279426807042594960959184304126581667
+                    256)))
+                 (bv #x0000000000000000 64)))))
+             (:union: (bv 0 32) null))
+            (vector))))))
+    (define/provide-test-suite serialize/test
+      (test-case
+       "serialize transaction"
+       (check-equal?
+        (serialize example-1)
+        '(ENVELOPE-TYPE-TX
+          ((KEY-TYPE-MUXED-ED25519 0 (65 32 245 4 132 15 130 86 201 188 205 227 214 17 246 138 208 74 110 187 8 101 242 142 164 42 213 13 113 216 91 163))
+           0
+           0
+           (FALSE . void)
+           (MEMO-RETURN 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+           #(((FALSE . void) (CREATE-ACCOUNT (PUBLIC-KEY-TYPE-ED25519 65 32 245 4 132 15 130 86 201 188 205 227 214 17 246 138 208 74 110 187 8 101 242 142 164 42 213 13 113 216 91 163) 0)))
+           (0 . void))
+          #())))))
