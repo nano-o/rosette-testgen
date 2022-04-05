ce2fac47f11633ddadeef1969ad7550eda25c38b
Giuliano Losa <giuliano@losa.fr>
2022-04-05 10:25:02 -0700

    add sign.rkt

---
+++ sign.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(require shell/pipeline)
+(provide sign scm-tx->base64)
+
+; Sign a transaction using stc in a ephemeral docker container
+
+(define docker-image "testgen-utils:latest")
+
+; -i has to be quoted or Racket reads it as a complex number:
+(define docker-run-prefix `(docker run "-i" --rm ,docker-image))
+
+(define (scm-tx->base64 scm-tx)
+  (run-subprocess-pipeline/out
+   `(,(λ () (write scm-tx))) ; write as datum that can be read back by a guile script
+   `(,@docker-run-prefix serialize.sh TransactionEnvelope)))
+
+(define (sign base64-tx private-key)
+  (let ([output
+          (run-subprocess-pipeline/out
+           `(,(λ () (printf "~a" base64-tx)))
+           `(,@docker-run-prefix sign.sh ,private-key))])
+    (string-trim output "\n")))
+
+(module+ test
+  (require rackunit)
+  (define/provide-test-suite sign/test
+    (test-case
+     "sign a transaction"
+     ; some test data
+     (define priv "SCI3M6F4CNX6A4RBPGZDJDYZGZSEF7ILYZJRT2FMXH3IYEQAHGYPWSBU")
+     (define scm-tx '(ENVELOPE-TYPE-TX ((KEY-TYPE-ED25519 44 116 101 211 153 7 145 197 247 66 94 199 157 227 123 109 182 170 88 99 83 127 201 141 20 216 160 78 16 173 253 84) 0 0 (FALSE . void) (MEMO-RETURN 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) #(((FALSE . void) (CREATE-ACCOUNT (PUBLIC-KEY-TYPE-ED25519 154 45 129 241 229 195 238 19 247 0 205 78 165 37 151 211 171 98 169 138 169 253 207 0 156 47 109 174 50 121 132 134) 10000000))) (0 . void)) #()))
+     (define signed-tx "AAAAAgAAAAAsdGXTmQeRxfdCXsed43tttqpYY1N/yY0U2KBOEK39VAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAmi2B8eXD7hP3AM1OpSWX06tiqYqp/c8AnC9trjJ5hIYAAAAAAJiWgAAAAAAAAAABEK39VAAAAECgKT1XbANJwFUUG7wUc2szI6JrJNMWtWKCGoEkVBZHNSCpXUmFkyP5TiIADnQHaVGvM59ixpIqNE/YP6MMYu8D")
+     (check-equal?
+      (sign (scm-tx->base64 scm-tx) priv) signed-tx))))
+      
--- tests.rkt
+++ tests.rkt
@@ -7,6 +7,7 @@
   (submod "key-utils.rkt" test)
   (submod "serialize.rkt" test)
   (submod "path-explorer.rkt" test)
+  (submod "sign.rkt" test)
   rackunit/text-ui)
 
 (define (get-test-exports m)
@@ -36,3 +37,4 @@
 (run-all "key-utils.rkt")
 (run-all "serialize.rkt")
 (run-all "path-explorer.rkt")
+(run-all "sign.rkt")
