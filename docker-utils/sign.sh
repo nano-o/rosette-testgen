f079039a0be56444689bc125a1251932e0fba167
Giuliano Losa <giuliano@losa.fr>
2022-04-08 17:39:32 -0700

    add stc config

--- docker-utils/sign.sh
+++ docker-utils/sign.sh
@@ -2,4 +2,4 @@
 # Expects a private key in strkey format as single argument.
 # Expects a transaction encoded in base64 on stdin.
 echo $1 | stc -import-key "the-key"
-cat | stc -sign -key "the-key" -c -
+cat | stc -sign -net test -key "the-key" -c -
