5cec1c452e0277472e6e41c1a7814bc5df8dd9c4
Giuliano Losa <giuliano@losa.fr>
2022-06-15 10:30:09 -0700

    rename

--- xdr-compiler-test.rkt
+++ xdr-compiler-test.rkt
@@ -2,24 +2,17 @@
 
 (require
   (for-syntax
-    (only-in racket pretty-print)
-    racket/set
+    ; (only-in racket pretty-print)
+    ; racket/set
     (only-in "xdr-compiler.rkt" xdr-types->racket)
     (only-in "Stellar-overrides.rkt" overrides)
     "read-datums.rkt")
-  ; "read-datums.rkt"
-  ; (only-in "Stellar-overrides.rkt" overrides)
   (only-in rosette bv)
-  ; lens
-  ; unstable/lens
   rackunit)
 
 (define-for-syntax
   Stellar-xdr-types
   (read-datums "./Stellar.xdr-types"))
-; (define
-  ; Stellar-xdr-types
-  ; (read-datums "./Stellar.xdr-types"))
 
 (define-syntax (compile-xdr stx)
   (define the-syntax
@@ -29,7 +22,6 @@
       stx
       ; (list "TransactionEnvelope" "TestLedger" "TestCaseResult")))
       (list "AccountEntry")))
-  ; (pretty-print (syntax->datum the-syntax))
   the-syntax)
 
 (compile-xdr)
@@ -38,13 +30,6 @@
   (define x/256 (-byte-array (bv 0 256)))
 
   (define my-pubkey (PublicKey (bv PUBLIC_KEY_TYPE_ED25519 32) x/256))
-
-  ; (define x-again (lens-view PublicKey-value-lens my-pubkey))
-  ; (check-true (bveq (-byte-array-value x/256) (-byte-array-value x-again)))
-  ; (check-true
-    ; (bveq
-      ; (lens-view -byte-array-value-lens x/256)
-      ; (lens-view~> my-pubkey PublicKey-value-lens -byte-array-value-lens)))
 
   (test-case
     "PublicKey-valid?"
